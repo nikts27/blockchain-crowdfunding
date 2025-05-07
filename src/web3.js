54c10c0ed8030fbd37acd4c82f7a2c4aa0ff4d25
nikts27 <nikostsaridis227@gmail.com>
2025-05-07 16:55:11 +0300

    Add frontend code

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,6 @@
 import Web3 from 'web3';
 
-const web3 = (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') 
-  ? new Web3(window.ethereum) 
-  : new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545')); // Ganache URL
+const web3 = new Web3(window.ethereum);
 
 export default web3;
+
